44de0de8a6333c902ebf1fbf6df426099fa1fe8d
Liliane <cpaixaoliliane@gmail.com>
2023-10-13 20:25:27 -0300

    Included animations

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
-let img = document.querySelector("#imgBis")
-let imgC = document.querySelector("#imgBisCra")
-let screen1 = document.querySelector(".screen1")
-let screen2 = document.querySelector(".screen2")
-let button = document.querySelector("#btnBack")
+const fortuneCookie = document.querySelector("#fortuneCookie")
+
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+let tryAgain = document.querySelector("#tryAgain")
+
+const fortune =[
+  "O aprendizado é como o horizonte: não há limites.",
+  "Não há que ser forte, há que ser flexível.",
+  "Limitações são fronteiras criadas apenas pela nossa mente.",
+  "O cão não ladra por valentia e sim por medo.",
+  "Procure acender uma vela em vez de amaldiçoar a escuridão.",
+  "A palavra é prata, o silêncio é ouro.",
+  "Lembre-se de que grandes realizações e grandes amores envolvem grandes riscos.",
+  "Um pouco de perfume sempre fica nas mãos de quem oferece flores.",
+  "O homem só envelhece quando os lamentos substituem seus sonhos.",
+  "A persistência realiza o impossível.",
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+]
 
 
-img.addEventListener('click', handleCraTex)
-button.addEventListener('click', toggleSreen)
+fortuneCookie.addEventListener('click', handleTryClick)
+tryAgain.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter' && screen2.classList.contains('hide')){
+    this.hasChildNodes()
+  } else if(e.key == 'Enter' && screen1.classList.contains('hide')){
+    handleResetClick()
+  }
+}
+)
 
 
-function handleCraTex(event){
+function handleTryClick(event){
  toggleSreen()
+ pickupFortune()
+}
+
+function handleResetClick(){
+  toggleSreen()
+}
+
+function pickupFortune(){
+  let allFortunes = fortune.length
+  let randomNumber = Math.floor(Math.random() * allFortunes)
+  screen2.querySelector("h3").innerText = `${fortune[randomNumber]}`
 }
 
 function toggleSreen(){
